21382c3feca3eaa9353bfab7f0fc09d39486d025
Rodrigo Teixeira Silva <60649228+rodrigo-teixeira-silva@users.noreply.github.com>
2024-07-16 10:11:04 -0300

    Update test_first_script.py

--- test_first_script.py
+++ test_first_script.py
@@ -1,33 +1,36 @@
 from asyncio import wait
+import select
 from selenium import webdriver
+
 from selenium.webdriver.support.select import Select
-from selenium.webdriver.chrome.service import Service as ChromeService
+
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+
 import time
 
-browser = webdriver.Chrome() 
+servico = Service(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=servico) 
 browser.implicitly_wait(10)
 browser.maximize_window()
 browser.get("https://app.kodigos.com.br/scm/front/")
 
 browser.find_element(By.XPATH,'//*[@id="app"]/div/div/div/div/div/div/div[1]/aside/div/div/div[1]/div/div/a[2]').click()
+time.sleep(3)
 
 browser.find_element(By.XPATH,'//*[@id="app"]/div/div/div/div/div/div/div[2]/main/div/div[3]/div[2]/label/div[2]/a/span[2]').click()
-time.sleep(5)
-
-browser.find_element(By.XPATH,'//*[@id="app"]/div/div/div/div/div/div/div[2]/main/div/div[3]/div[2]/label/div/div/div[2]/i').click()
 time.sleep(3)
 
 
-#Esperar até que o campo "Filial" esteja disponível e clicar nele
-# wait = WebDriverWait(driver, 10)
-# filial_field = wait.until(EC.element_to_be_clickable((By.ID, 'f_952a7000-e715-4da7-8949-35d2f9056cf9')))
-# filial_field.click()
-# time.sleep(10)
-# Esperar até que o menu dropdown esteja disponível e selecionar "01/01 - Instituto Kodigos"
-#browser = wait.Select(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div/div/div/div/div/div/div[2]/main/div/div[3]/div[2]/label/div/div/div[1]'))).select_by_visible_text('01/01 - Instituto Kodigos')
-# browser.click()
-time.sleep(10)
+dropdown_filial = Select(browser.find_element(By.XPATH, '//div[@class="dropdown-menu"]')) 
+dropdown_filial.select_by_value('01/01 - Instituto Kodigos')
+time.sleep(3)
+
+
+# input de texto
+browser.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div/div/div[2]/main/div/div[4]/div[2]/label').send_keys('Rodrigo_teixeira')
+time.sleep(3)
+
 
